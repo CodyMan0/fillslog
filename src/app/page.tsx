d40c1104678606eace7a678aa9a1191d3c262bc4
CodyMan0 <hys83751952@gmail.com>
2025-04-14 08:40:49 +0900

    [DESIGN] 드럼 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import Drum from "@/shared/assets/images/drum.png";
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <h1>Fills log</h1>
+      <Image src={Drum} alt="hero_drum" />
+    </div>
+  );
 }
