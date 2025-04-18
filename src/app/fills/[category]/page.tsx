145446a5e63897673f66148abd696457fad9bde1
CodyMan0 <hys83751952@gmail.com>
2025-04-18 14:21:14 +0900

    [FEAT] Floating 컴포넌트 구현

--- src/app/fills/[category]/page.tsx
+++ src/app/fills/[category]/page.tsx
@@ -1,20 +1,14 @@
 import BottomNav from "@/app/components/MobileNav";
 import { FillCard } from "@/features/fills";
+import FillFab from "@/features/fills/FloatingFillButton";
 import PageLayout from "@/features/layouts/PageLayout";
-import { Button } from "@/shared/ui";
-import Link from "next/link";
+
 import React from "react";
 
 const Page = () => {
   return (
     <PageLayout>
       <div>
-        <Button
-          className="w-full bg-white rounded-full text-black hover:scale-105 transition-all duration-200 ease-in-out mt-4"
-          asChild
-        >
-          <Link href="/fills/add">필인 줍줍</Link>
-        </Button>
         <FillCard
           title="4연음 트릴"
           bpm={140}
@@ -27,18 +21,7 @@ const Page = () => {
           videoUrl="https://youtube.com"
           description="느린 발라드에서 리드인용으로 사용"
         />
-        <FillCard
-          title="4연음 트릴"
-          bpm={90}
-          videoUrl="https://youtube.com"
-          description="느린 발라드에서 리드인용으로 사용"
-        />
-        <FillCard
-          title="4연음 트릴"
-          bpm={90}
-          videoUrl="https://youtube.com"
-          description="느린 발라드에서 리드인용으로 사용"
-        />
+        <FillFab />
         <BottomNav />
       </div>
     </PageLayout>
