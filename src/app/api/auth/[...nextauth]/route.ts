80e7712a5b890acdc1fc70b44911a5e0c5dd31b7
CodyMan0 <hys83751952@gmail.com>
2025-04-18 15:31:32 +0900

    [FEAT] 미들 웨어 동작하도록 폴더 위치 변경

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,4 @@ import { authOptions } from "@/shared/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
-console.log("handler", handler);
 export { handler as GET, handler as POST };
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,29 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req }); // next-auth 토큰 가져옴
+  const token = await getToken({
+    req,
+    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+  }); // next-auth 토큰 가져옴
+  console.log("[middleware] 실행됨, token:", token);
   const isAuth = !!token;
 
   const pathname = req.nextUrl.pathname;
 
   const isProtectedPage = pathname.startsWith("/fills");
+  const isLandingPage = pathname === "/";
 
   // 1. 로그인 안 한 사람이 /fills 접근하면 → /
   if (!isAuth && isProtectedPage) {
     return NextResponse.redirect(new URL("/", req.url));
   }
+  // 2. 로그인 한 사람이 / 접근하면 → /fills
+  if (isAuth && isLandingPage) {
+    return NextResponse.redirect(new URL("/fills", req.url));
+  }
 
   return NextResponse.next(); // 통과
 }
-
 export const config = {
-  matcher: ["/", "/login", "/fills"],
+  matcher: ["/", "/fills/:path*"],
 };
