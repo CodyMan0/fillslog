e48bc2fed17ee0d6217f5c5ca44f3d9d33dc0baa
CodyMan0 <hys83751952@gmail.com>
2025-04-18 17:31:56 +0900

    [FIX] mock data 타입 에러

--- src/app/fills/[category]/page.tsx
+++ src/app/fills/[category]/page.tsx
@@ -1,3 +1,4 @@
+import BottomNav from "@/app/components/MobileNav";
 import { FillCard } from "@/features/fills";
 import FillFab from "@/features/fills/FloatingFillButton";
 import Header from "@/features/layouts/Header";
@@ -5,9 +6,15 @@ import PageLayout from "@/features/layouts/PageLayout";
 
 import React from "react";
 
-const mock = [];
+const mock = [
+  {
+    title: "필인 제목",
+    bpm: 120,
+    videoUrl: "https://www.youtube.com/watch?v=abc123",
+    description: "필인 설명",
+  },
+];
 
-// const emptyMock = [];
 const Page = () => {
   return (
     <PageLayout>
@@ -16,19 +23,9 @@ const Page = () => {
       </Header>
 
       <div className="flex flex-col gap-2">
-        {mock.length === 0 ? (
-          <div className="flex flex-col items-center justify-center h-full">
-            <div className="text-2xl font-bold">아직 등록된 필인이 없어요</div>
-            <div className="text-sm text-text-secondary mt-2">
-              <span className="text-accent-blue font-bold">
-                금방 머리속에서 떠나가는 필인
-              </span>
-              을 저장해보세요!
-            </div>
-          </div>
-        ) : (
+        {mock.map((fill) => (
           <>
-            {mock.map((fill) => (
+            {fill.title ? (
               <FillCard
                 key={fill.title}
                 title={fill.title}
@@ -36,11 +33,24 @@ const Page = () => {
                 videoUrl={fill.videoUrl}
                 description={fill.description}
               />
-            ))}
+            ) : (
+              <div className="flex flex-col items-center justify-center h-full">
+                <div className="text-2xl font-bold">
+                  아직 등록된 필인이 없어요
+                </div>
+                <div className="text-sm text-text-secondary mt-2">
+                  <span className="text-accent-blue font-bold">
+                    금방 머리속에서 떠나가는 필인
+                  </span>
+                  을 저장해보세요!
+                </div>
+              </div>
+            )}
           </>
-        )}
+        ))}
 
         <FillFab />
+        <BottomNav />
       </div>
     </PageLayout>
   );
