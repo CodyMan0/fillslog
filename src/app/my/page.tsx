3eef7e90775071b9b3d6a15758fe1a24aa2e723e
CodyMan0 <hys83751952@gmail.com>
2025-04-18 14:50:05 +0900

    [DESIGN] 마이페이지 헤더 적용

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,12 +1,23 @@
 import PageLayout from "@/features/layouts/PageLayout";
 import BottomNav from "../components/MobileNav";
+
+import Header from "@/features/layouts/Header";
+import Profile from "./components/Profile";
 import ComingSoon from "../components/ComingSoon";
 
 const page = () => {
   return (
     <PageLayout>
       <div>
+        <Header>
+          <Header.Previous />
+          <Header.Text text="My Page" />
+        </Header>
+
+        <Profile />
+
         <ComingSoon />
+
         <BottomNav />
       </div>
     </PageLayout>
